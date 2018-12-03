8a22f7c97765728010aa7c6eaf3d77ccef28a72a
xiangqingxue <77055664@qq.com>
2018-12-03 10:31:52 +0800

    update
    Signed-off-by: xiangqingxue <77055664@qq.com>

--- src/main/java/cn/authDemo/controller/TestController.java
+++ src/main/java/cn/authDemo/controller/TestController.java
@@ -2,12 +2,19 @@
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
  public class TestController {
 
     @RequestMapping("/hello")
     public String hello() {
+        
+        return "hello";
+    }
+    @RequestMapping("/stringtest")
+    @ResponseBody
+    public String stringTest() {
         
         return "hello";
     }
