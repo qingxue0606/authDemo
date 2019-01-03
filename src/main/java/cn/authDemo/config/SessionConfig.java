4d81082fd49ce60cb22e3ce2f7ca7c41fb327dd4
xiangqingxue <77055664@qq.com>
2019-01-03 16:46:52 +0800

    update
    Signed-off-by: xiangqingxue <77055664@qq.com>

---
+++ src/main/java/cn/authDemo/config/SessionConfig.java
@@ -0,0 +1,22 @@
+ package cn.authDemo.config;
+
+import org.springframework.context.annotation.Bean;
+//import org.springframework.session.data.mongo.JdkMongoSessionConverter;
+//import org.springframework.session.data.mongo.config.annotation.web.http.EnableMongoHttpSession;
+//import org.springframework.session.web.http.CookieSerializer;
+//import org.springframework.session.web.http.DefaultCookieSerializer;
+
+/**
+ * 整合mangodb
+ */
+/*@EnableMongoHttpSession
+public class SessionConfig {
+    
+    @Bean
+    public JdkMongoSessionConverter mongoSessionConverter() {
+        return new JdkMongoSessionConverter(); 
+    }
+
+
+    
+}*/
--- src/main/java/cn/authDemo/controller/TestController.java
+++ src/main/java/cn/authDemo/controller/TestController.java
@@ -1,5 +1,9 @@
  package cn.authDemo.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,8 +12,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
  public class TestController {
 
     @RequestMapping("/hello")
-    public String hello() {
-        
+    public String hello( HttpSession session) {
+        UserDetails userDetails=(UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        session.setAttribute("xiang", 111);
+        System.out.println(session.getAttribute("xiang"));
+        System.out.println(userDetails.getAuthorities());
         return "hello";
     }
     @RequestMapping("/stringtest")
